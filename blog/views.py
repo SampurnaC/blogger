839b1a3801b2f2112c31ea5d541871339dbfd60e
sam <sampurna.chapagainnn@gmail.com>
2023-05-26 19:27:56 +0100

    create new app and work on urls and render the template

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world!") 
+
+    posts = ['test1', 'test2', 'test3']  
+
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+def about(request):
+    return render(request, 'blog/about.html')
