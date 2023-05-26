d06c6f83f1dc01ce45f5f81810db5e74f3f5a4ff
sam <sampurna.chapagainnn@gmail.com>
2023-05-26 21:50:59 +0100

    work on create function

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from blog.forms import BlogForm
+from blog.models import Blog
 
 # Create your views here.
 
@@ -11,3 +13,13 @@ def index(request):
 
 def about(request):
     return render(request, 'blog/about.html')
+
+def new(request):
+    if request.method == "POST":
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/about')
+    else:
+        form=BlogForm()
+    return render(request, 'blog/index.html', {'form': form})            
