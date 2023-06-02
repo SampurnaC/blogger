7bd894100d7841fb75ccb0dabb66a629da9eb55c
sam <sampurna.chapagainnn@gmail.com>
2023-06-02 20:53:26 +0100

    add url and forms for signup page

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from .forms import SignupForm
 # Create your views here.
+
+def signup(request):
+    form=SignupForm()
+
+    return render(request, 'users/signup.html', {
+        'form': form
+    })
